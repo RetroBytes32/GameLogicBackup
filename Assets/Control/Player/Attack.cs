eb4c37faf7d9bcf64ed8be1efd59ca5a93e5d083
RetroBytes32 <66331916+RetroBytes32@users.noreply.github.com>
2023-08-30 21:34:47 -0600

    Add files via upload

---
+++ Assets/Control/Player/Attack.cs
@@ -0,0 +1,455 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Attack : MonoBehaviour {
+	
+	[Space(10)]
+    [Header("Masks")]
+	[Space(5)]
+    
+	public LayerMask AttackMask;
+	public LayerMask EntityMask;
+	public LayerMask GroundMask;
+	
+	
+	[Space(10)]
+    [Header("Attack timer")]
+	[Space(5)]
+    
+	public int    attackTimeout = 0;
+	public int    attackTimer   = 0;
+	public bool   canAttack     = false;
+	
+	
+	[Space(10)]
+    [Header("Player base melee damage")]
+	[Space(5)]
+    
+	public int       playerAttackDamage  = 1;
+	public int       playerstrikingForce = 1;
+	public string    playerTargetItemClass;
+	
+	public float     Distance            = 7.0f;
+	
+	
+	[Space(10)]
+    [Header("Attack animation")]
+	[Space(5)]
+    
+    public float  attackAnimationSwingRate      = 8.0f;
+    public float  attackAnimationRetractRate    = 3.0f;
+    public float  attackCooloffTimer            = -0.9f;
+    
+	public float  attackAnimationX              = 1.4f;
+	public float  attackAnimationY              = 1.4f;
+	public float  attackAnimationZ              = 1.4f;
+	
+	
+	
+	[Space(10)]
+    [Header("Pickup animation")]
+	[Space(5)]
+    
+	public float  pickupAnimationX              = 1.4f;
+	public float  pickupAnimationY              = 1.4f;
+	public float  pickupAnimationZ              = 1.4f;
+	
+	
+	
+	
+	Interface interfaceScript;
+	
+	Inventory inventory;
+	
+	Camera cameraObject;
+	
+	TickUpdate tickUpdate;
+	
+	bool  runAttackAnimCycle = false;
+	bool  runPickupAnimCycle = false;
+	
+	// Animation cycle offsets
+	bool  attackOffsetDirection = false;
+	float attackOffset;
+	
+	
+	
+	
+	
+	
+	
+	void Update () {
+        
+        if (tickUpdate.isPaused) return;
+        if (tickUpdate.doShowConsole) return;
+        
+        
+        // Decrement attack timer
+        if (attackTimer > 0) 
+            attackTimer--;
+        
+        
+        if (Input.GetButtonDown("Attack")) {
+            
+            GetComponent<Use>().resetConsumeAnimation();
+            
+            string inHandItem = inventory.checkSlot();
+            
+            playerAttackDamage     = 1;
+            playerstrikingForce      = 1;
+            playerTargetItemClass  = "";
+            
+            // Check current weapon melee damage amount
+            for (int i=0; i < tickUpdate.weaponItem.Length; i++) {
+                if (tickUpdate.weaponItem[i].name != inHandItem) 
+                    continue;
+                
+                playerAttackDamage    = tickUpdate.weaponItem[i].attackDamage;
+                playerstrikingForce   = tickUpdate.weaponItem[i].strikingForce;
+                playerTargetItemClass = tickUpdate.weaponItem[i].targetItemClass;
+                
+            }
+            
+        }
+        
+        
+        if (Input.GetButtonUp("Attack")) {
+            
+            GetComponent<Use>().resetConsumeAnimation();
+            
+            attackTimer = 0;
+            
+            transform.GetChild(0).transform.GetChild(0).transform.GetChild(0).transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+        
+        
+        if (Input.GetButton("Attack")) {
+            
+            
+            //
+            // Ray check pickup objects
+            
+            RaycastHit hit_obj;
+            Ray ray_obj = cameraObject.ScreenPointToRay(Input.mousePosition);
+            
+            if ( Physics.Raycast(ray_obj, out hit_obj, Distance, AttackMask) ) {
+                
+                // Get object name
+                string objectName = hit_obj.transform.parent.transform.gameObject.name;
+                GameObject targetObject = hit_obj.transform.parent.transform.gameObject;
+                
+                if (objectName == "hitbox") {
+                    objectName   = hit_obj.transform.parent.transform.parent.transform.gameObject.name;
+                    targetObject = hit_obj.transform.parent.transform.parent.transform.gameObject;
+                }
+                
+                //
+                // Attack time out
+                
+                if (attackTimer == 1) {
+                    
+                    // Remove some durability
+                    if (interfaceScript.inventory.Durability[interfaceScript.selectedSlot] > 0) 
+                        interfaceScript.inventory.Durability[interfaceScript.selectedSlot]--;
+                    
+                    attackTimer = 0;
+                    
+                    runAttackAnimCycle = false;
+                    
+                    transform.GetChild(0).transform.GetChild(0).transform.GetChild(0).transform.localPosition = new Vector3(1.5f, -1f, 2f);
+                    transform.GetChild(0).transform.GetChild(0).transform.GetChild(0).transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+                    
+                    // Check if the item is shatterable
+                    for (int i=0; i < tickUpdate.shatterableItem.Length; i++) {
+                        if (tickUpdate.shatterableItem[i].name != objectName) 
+                            continue;
+                        
+                        // Destroy the item
+                        Destroy( targetObject );
+                        
+                        
+                        // Remove object from the structure
+                        if (tickUpdate.chunkGenerator.doSavePlacedObjectsToStructure) 
+                            tickUpdate.removeObjectFromStructure( hit_obj.transform.position );
+                        
+                        return;
+                    }
+                    
+                    // Pick up the item
+                    runPickupAnimCycle = true;
+                    
+                    // Get max stack count
+                    int maxStack=0;
+                    for (int i=0; i < tickUpdate.items.Length; i++) {
+                        if (tickUpdate.items[i].name != objectName) 
+                            continue;
+                        maxStack = tickUpdate.items[i].stackMax;
+                    }
+                    
+                    // Pick up the item
+                    Destroy( targetObject );
+                    inventory.addItem(objectName, 1, maxStack, -1);
+                    interfaceScript.updateInHand();
+                    
+                    // Remove object from the structure
+                    if (tickUpdate.chunkGenerator.doSavePlacedObjectsToStructure) 
+                        tickUpdate.removeObjectFromStructure( hit_obj.transform.position );
+                    
+                    return;
+                }
+                
+                
+                //
+                // Begin attack delay
+                
+                if (attackTimer < 1) {
+                    
+                    // Get max stack count
+                    int maxStack=0;
+                    for (int i=0; i < tickUpdate.items.Length; i++) {
+                        if (tickUpdate.items[i].name != objectName) 
+                            continue;
+                        
+                        maxStack = tickUpdate.items[i].stackMax;
+                    }
+                    
+                    // Check if the item requires breaking delay
+                    for (int i=0; i < tickUpdate.breakableItem.Length; i++) {
+                        if (tickUpdate.breakableItem[i].name != objectName) 
+                            continue;
+                        
+                        attackTimer = tickUpdate.breakableItem[i].hardness;
+                        
+                        // Check target class
+                        if (playerTargetItemClass == tickUpdate.breakableItem[i].itemClass) {
+                            
+                            attackTimer -= playerstrikingForce;
+                            
+                        } else {
+                            
+                            // Default target ALL item classes
+                            if (playerTargetItemClass == "") 
+                                attackTimer -= playerstrikingForce;
+                        }
+                        
+                        runAttackAnimCycle = true;
+                        runPickupAnimCycle = false;
+                        
+                        if (attackTimer < attackTimeout) 
+                            attackTimer = attackTimeout;
+                        
+                        return;
+                    }
+                    
+                    // Check if the item is shatterable
+                    for (int i=0; i < tickUpdate.shatterableItem.Length; i++) {
+                        if (tickUpdate.shatterableItem[i].name != objectName) 
+                            continue;
+                        
+                        attackTimer = tickUpdate.shatterableItem[i].hardness;
+                        
+                        runAttackAnimCycle = true;
+                        runPickupAnimCycle = false;
+                        
+                        if (attackTimer < attackTimeout) 
+                            attackTimer = attackTimeout;
+                        
+                        return;
+                    }
+                    
+                    // No delay on pick up
+                    attackTimer = 0;
+                    
+                    runAttackAnimCycle = false;
+                    runPickupAnimCycle = true;
+                    
+                    // Get item data
+                    ItemTag itemTag = targetObject.GetComponent<ItemTag>();
+                    
+                    int durability  = -1;
+                    
+                    string durabilityTag = itemTag.getTag("durability");
+                    if (durabilityTag != "") {
+                        
+                        durability = int.Parse(durabilityTag);
+                    }
+                    
+                    // Pick up the item
+                    Destroy( targetObject );
+                    inventory.addItem(objectName, 1, maxStack, durability);
+                    interfaceScript.updateInHand();
+                    
+                    // Remove object from the structure
+                    if (tickUpdate.chunkGenerator.doSavePlacedObjectsToStructure) 
+                        tickUpdate.removeObjectFromStructure( hit_obj.transform.position );
+                    
+                } else {
+                    
+                    runAttackAnimCycle = true;
+                }
+                
+            }
+            
+            
+            
+            
+            
+            //
+            // Ray check attack entities
+            
+            RaycastHit hit_ent;
+            Ray ray_entity = cameraObject.ScreenPointToRay(Input.mousePosition);
+            
+            if ( Physics.Raycast(ray_entity, out hit_ent, Distance, EntityMask) ) {
+                
+                string objectName = hit_ent.transform.gameObject.name;
+                
+                
+                //
+                // Attack entity then delay
+                
+                if (attackTimer <= 0) {
+                    attackTimer = attackTimeout;
+                }
+                
+                if (attackTimer == attackTimeout) {
+                    
+                    runAttackAnimCycle = true;
+                    
+                    EntityTag entityTag = hit_ent.transform.gameObject.GetComponent<EntityTag>();
+                    ActorTag  actorTag  = hit_ent.transform.gameObject.GetComponent<ActorTag>();
+                    
+                    if (entityTag == null) return;
+                    if (actorTag == null) return;
+                    
+                    //
+                    // Calculate and apply attack damage
+                    
+                    entityTag.Health -= playerAttackDamage;
+                    
+                    actorTag.isInPain = true;
+                    actorTag.targetEntity = gameObject;
+                    
+                    actorTag.fear   = 40;
+                    actorTag.stress = 100;
+                    
+                    if (actorTag.chanceToAttackPlayer > 0f) 
+                        actorTag.AI_attack_player(20);
+                    
+                    if (entityTag.Health < 0) {
+                        
+                        if (!actorTag.isDying) 
+                            actorTag.isDying = true;
+                        
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+        //
+        // Attack animation cycle
+        
+        if (runAttackAnimCycle == true) {
+            
+            // Switch from swing to retract
+            if (attackOffsetDirection == false) {
+                // Swing rate
+                attackOffset += attackAnimationSwingRate;
+            } else {
+                // Retract rate
+                attackOffset -= attackAnimationRetractRate;
+            }
+            
+            // Retract back
+            if (attackOffset < 0f) {
+                
+                if (attackOffset < attackCooloffTimer) {
+                    attackOffset = 0f;
+                    attackOffsetDirection = false;
+                    runAttackAnimCycle    = false;
+                }
+                
+                transform.GetChild(0).transform.GetChild(0).transform.GetChild(0).transform.localPosition = new Vector3(1.5f, -1f, 2f);
+                transform.GetChild(0).transform.GetChild(0).transform.GetChild(0).transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+                
+                return;
+            }
+            
+            // Swing out
+            if (attackOffset > 1.1f) {
+                attackOffset = 1.1f;
+                attackOffsetDirection = true;
+            }
+            
+            transform.GetChild(0).transform.GetChild(0).transform.GetChild(0).transform.localRotation = Quaternion.Euler(0f, -20f, 0f);
+            
+            transform.GetChild(0).transform.GetChild(0).transform.GetChild(0).transform.localPosition = new Vector3(1.5f - (attackOffset * attackAnimationX ),
+                                                                                                                     -1f - (attackOffset * attackAnimationY ), 
+                                                                                                                      2f + (attackOffset * attackAnimationZ ));
+            
+            
+        }
+        
+        
+        
+        //
+        // Item pickup animation cycle
+        
+        if (runPickupAnimCycle == true) {
+            
+            attackOffset += 0.8f;
+            
+            
+            if (attackOffset > 3f) {
+                attackOffset = 3f;
+                attackOffsetDirection = false;
+                
+                transform.GetChild(0).transform.GetChild(0).transform.GetChild(0).transform.localPosition = new Vector3(1.5f, -1f, 2f);
+                
+                runPickupAnimCycle = false;
+                attackOffset = 0f;
+                return;
+            }
+            
+            transform.GetChild(0).transform.GetChild(0).transform.GetChild(0).transform.localPosition = new Vector3(1.5f - (attackOffset * pickupAnimationX),
+                                                                                                                     -1f - (attackOffset * pickupAnimationY),
+                                                                                                                      2f + (attackOffset * pickupAnimationZ));
+            
+            transform.GetChild(0).transform.GetChild(0).transform.GetChild(0).transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+            
+        }
+        
+        
+        
+        
+        return;
+	}
+	
+	
+	
+	
+	void Start () {
+        
+        tickUpdate = GameObject.Find("GameRules").GetComponent<TickUpdate>();
+        
+        inventory = GameObject.Find("HUD").GetComponent<Inventory>();
+        interfaceScript = GameObject.Find("HUD").GetComponent<Interface>();
+        
+        cameraObject = GameObject.Find("Main Camera").GetComponent<Camera>();
+        
+        
+	}
+	
+	
+}
---
+++ Assets/Control/Player/Movement.cs
@@ -0,0 +1,243 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+	
+	public CharacterController controller;
+	public float Speed         = 10f;
+	public float Gravity       = 0.5f;
+	public float JumpForce     = 3f;
+	
+    public float flyCamSpeed                = 7f;
+	public float flyCamAcceleration         = 4f;
+    public float flyCamMimimumSpeedCutoff   = 0.1f;
+    public float flyCamDamping              = 0.9f;
+    
+    public Transform  groundCheck;
+	public float      groundDistance = 0.5f;
+	public LayerMask  groundMask;
+	
+	public bool  readyToJump   = false;
+	public int   jumpTimer     = 0;
+	public int   jumpCoolDown  = 7;
+	
+	public bool isGrounded    = false;
+	
+	public Transform PlayerSourceTransform;
+	
+	public Vector3 Velocity;
+	
+	public float  camearFov = 60f;
+	public float  fovOffset = 0;
+	
+	public TickUpdate tickUpdate;
+	
+	public bool isDoubleFast = false;
+	
+	
+	
+	void Update () {
+        
+        //
+        // Normal mode
+        //
+        
+        if (!tickUpdate.doDebugMode) {
+            
+            float doubleFast   = 1f;
+            
+            Vector3 Movement = new Vector3(0f, 0f, 0f);
+            
+            isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+            
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+            
+            // Check turn off double speed
+            if (!Input.GetKey(KeyCode.W)) 
+                isDoubleFast = false;
+            
+            if (!tickUpdate.doShowConsole) {
+                
+                // Check forward double fast
+                if (Input.GetKeyDown(KeyCode.LeftControl)) {
+                    
+                    isDoubleFast = true;
+                    doubleFast = 2.8f;
+                }
+                
+                // Camera FOV
+                if (isDoubleFast) {
+                    
+                    if (Random.Range(0, 10) == 1) 
+                        tickUpdate.inventory.removeSaturation(1);
+                    
+                    if (fovOffset < 6f) 
+                        fovOffset += 2;
+                } else {
+                    if (fovOffset > 0f) 
+                        fovOffset -= 2f;
+                }
+                
+                tickUpdate.cameraObject.fieldOfView = camearFov + fovOffset;
+                tickUpdate.Player.transform.GetChild(0).transform.GetChild(0).transform.gameObject.GetComponent<Camera>().fieldOfView = camearFov + fovOffset;
+                
+                
+                Movement = (PlayerSourceTransform.transform.right * x) + (PlayerSourceTransform.transform.forward * z);
+                // Apply movement force to the player controller
+                controller.Move(Movement * Speed * doubleFast * Time.deltaTime);
+                
+                // Start a jump when the player is on the ground
+                if ((Input.GetKey(KeyCode.Space)) & (isGrounded)) {
+                    
+                    // Jump begin
+                    if (readyToJump) {
+                        
+                        tickUpdate.inventory.removeSaturation(2);
+                        
+                        Velocity.y = JumpForce;
+                        
+                        isGrounded  = false;
+                        readyToJump = false;
+                        
+                        // Start the jump
+                        jumpTimer = 1;
+                    }
+                    
+                }
+                
+            }
+            
+            
+            // Apply gravity
+            if (!isGrounded) {
+                
+                Velocity.y += -Gravity;
+                
+            } else {
+                
+                Velocity.y = 0f;
+            }
+            
+            
+            // Jump cool down timer
+            if (jumpTimer > 0) {
+                jumpTimer ++;
+                
+                if (jumpTimer > jumpCoolDown) {
+                    jumpTimer = 0;
+                    
+                    readyToJump = true;
+                }
+            }
+            
+            
+            // Apply final velocity
+            controller.Move(Velocity * Speed * Time.deltaTime);
+            
+            
+            
+        //
+        // Fly cam mode
+        //
+        } else {
+            
+            float doubleFast = 1f;
+            
+            Vector3 Axis = new Vector3(0f, 0f, 0f);
+            
+            if (!tickUpdate.doShowConsole) {
+                
+                // Check forward double fast
+                if (Input.GetKeyDown(KeyCode.LeftControl)) 
+                    isDoubleFast = true;
+                
+                // Check directional velocity
+                if (Input.GetKey(KeyCode.A))           Axis.x = -1f;
+                if (Input.GetKey(KeyCode.D))           Axis.x =  1f;
+                if (Input.GetKey(KeyCode.Space))       Axis.y =  1f;
+                if (Input.GetKey(KeyCode.LeftShift))   Axis.y = -1f;
+                if (Input.GetKey(KeyCode.S))           Axis.z = -1f;
+                if (Input.GetKey(KeyCode.W))          {Axis.z =  1f;} else {
+                    
+                    // Turn off double speed
+                    isDoubleFast = false;
+                }
+                
+                // Check double fast
+                if (isDoubleFast) 
+                    doubleFast = 3f;
+            }
+            
+            // Apply movement force to the player controller
+            if (Axis != new Vector3(0f, 0f, 0f)) {
+                Vector3 newVelocity = ((PlayerSourceTransform.transform.right * Axis.x) + (PlayerSourceTransform.transform.forward * Axis.z)) * 
+                                       flyCamAcceleration * 
+                                       doubleFast;
+                
+                Velocity   += newVelocity;
+                Velocity.y += (Axis.y * flyCamAcceleration) * (doubleFast * 0.8f);
+            }
+            
+            // Camera FOV
+            if (isDoubleFast) {
+                if (fovOffset < 9f) 
+                    fovOffset += 3;
+            } else {
+                if (fovOffset > 0f) 
+                    fovOffset -= 3f;
+            }
+            
+            tickUpdate.cameraObject.fieldOfView = camearFov + fovOffset;
+            tickUpdate.Player.transform.GetChild(0).transform.GetChild(0).transform.gameObject.GetComponent<Camera>().fieldOfView = camearFov + fovOffset;
+            
+            
+            // Maximum speed cutoff
+            if (Velocity.x >  flyCamSpeed) Velocity.x =  flyCamSpeed;
+            if (Velocity.x < -flyCamSpeed) Velocity.x = -flyCamSpeed;
+            if (Velocity.y >  flyCamSpeed) Velocity.y =  flyCamSpeed;
+            if (Velocity.y < -flyCamSpeed) Velocity.y = -flyCamSpeed;
+            if (Velocity.z >  flyCamSpeed) Velocity.z =  flyCamSpeed;
+            if (Velocity.z < -flyCamSpeed) Velocity.z = -flyCamSpeed;
+            
+            // Damping
+            if (Velocity.x > 0f) Velocity.x *= flyCamDamping;
+            if (Velocity.x < 0f) Velocity.x *= flyCamDamping;
+            if (Velocity.y > 0f) Velocity.y *=(flyCamDamping * 0.95f);
+            if (Velocity.y < 0f) Velocity.y *=(flyCamDamping * 0.95f);
+            if (Velocity.z > 0f) Velocity.z *= flyCamDamping;
+            if (Velocity.z < 0f) Velocity.z *= flyCamDamping;
+            
+            
+            controller.Move(Velocity * Time.deltaTime);
+            
+            // Minimal speed cutoff
+            if ((Velocity.x > -flyCamMimimumSpeedCutoff) & (Velocity.x < flyCamMimimumSpeedCutoff)) Velocity.x = 0f;
+            if ((Velocity.y > -flyCamMimimumSpeedCutoff) & (Velocity.y < flyCamMimimumSpeedCutoff)) Velocity.y = 0f;
+            if ((Velocity.z > -flyCamMimimumSpeedCutoff) & (Velocity.z < flyCamMimimumSpeedCutoff)) Velocity.z = 0f;
+            
+        }
+        
+        return;
+	}
+	
+	
+	
+	
+	
+	void Start() {
+        
+        tickUpdate = GameObject.Find("GameRules").GetComponent<TickUpdate>();
+        
+	}
+	
+	
+}
+
+
+
+
+
+
+
